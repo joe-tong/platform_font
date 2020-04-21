a9911d1fd001a89d19299b293888c8d576efc9de
joe-tong <1918939227@qq.com>
2020-04-21 22:23:08 +0800

    任务发布平台基础架构

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+// 引用api
+import { login } from 'api/user'
+
+console.log(process.env.TEST)
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    /**
+     * [loadData axios示例]
+     * @return
+     */
+    // loadData() {
+    //   login({
+    //     uid: 1233
+    //   }).then(res => {
+    //       if (res.code === 200) {
+    //       }
+    //     })
+    //     .catch(err => {
+    //     })
+    // }
+  }
+}
+</script>
