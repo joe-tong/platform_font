5a1df895b2d32629d1341f7c9ca9d1d5b704f92e
joe-tong <1918939227@qq.com>
2020-04-24 23:15:04 +0800

    代码提交

---
+++ src/views/Demo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="demo">
+    <div id="head">
+      <span class="banner">图灵工作室|Tuling stadio</span>
+      <span class="person">个人中心</span>
+    </div>
+    <div id="body">
+      <div class="wx_common"></div>
+      <div class="wx_login"></div>
+    </div>
+    <div id="foot"></div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+#head {
+  height: 2.2em;
+  width: 100%;
+  background-color: rgb(143, 126, 104);
+  font-size: 1.5em;
+}
+.person {
+  padding-left: 45%;
+  line-height: 2em;
+  font-family: KaiTi;
+}
+.banner {
+  padding-left: 15%;
+  line-height: 2em;
+  font-family: KaiTi;
+}
+#body {
+  background-color: grey;
+  height: 39.5em;
+}
+#foot {
+  height: 2em;
+  background-color: red;
+}
+.boxShadow {
+  padding: 0 16px 10px;
+}
+.el-tabs__item {
+  color: #2f353b !important;
+  font-size: 16px !important;
+}
+
+</style>
+
+<script>
+export default {
+  methods: {}
+}
+</script>
+
---
+++ src/views/form/Login_form.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="login_form">
+    <el-form ref="form" :model="form" class="label_form">
+      <el-form-item label="账号" class="label_name">
+        <el-input class="input" v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" class="label_name">
+        <el-input class="input" v-model="form.pwd"></el-input>
+      </el-form-item>
+
+      <!-- <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>-->
+    </el-form>
+    <!-- <div class="title">账号登录</div> -->
+  </div>
+</template>
+
+<style lang="less" scoped>
+.label_form {
+  position: relative;
+}
+.input {
+  position: absolute;
+  left: 17%;
+  width: 18vw;
+}
+.label_name {
+  padding-left: 2em;
+  padding-top: 1.5em;
+  // position: absolute;
+  // left: 1%;
+  
+}
+</style>
+
+<script>
+export default {
+  name: 'login_form',
+  props: {},
+  data() {
+    return {
+      form: {
+        name: '',
+        pwd: ''
+      }
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    }
+  }
+}
+</script>
